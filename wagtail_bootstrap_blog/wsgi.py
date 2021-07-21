20fff837b59b1026e128518aaab2c645ff2571b1
Ваше Имя <you@example.com>
2021-07-21 13:31:51 +0800

    add

--- wagtail_bootstrap_blog/wsgi.py
+++ wagtail_bootstrap_blog/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wagtail_bootstrap_blog.settings.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wagtail_bootstrap_blog.settings.dev")
 
 application = get_wsgi_application()
