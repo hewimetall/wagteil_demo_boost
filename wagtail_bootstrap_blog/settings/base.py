8d32f3ce0b0884f51a86cbec09ad4715db9dac93
Ваше Имя <you@example.com>
2021-07-21 13:17:41 +0800

    add

--- wagtail_bootstrap_blog/settings/base.py
+++ wagtail_bootstrap_blog/settings/base.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'wagtail.contrib.routable_page',
     'wagtail.contrib.postgres_search',
     'wagtail.contrib.modeladmin',
-    
+
     'modelcluster',
     'taggit',
     'django_extensions',
@@ -107,15 +107,12 @@ WSGI_APPLICATION = 'wagtail_bootstrap_blog.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
-        "USER": os.environ.get("SQL_USER", "user"),
-        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
-        "HOST": os.environ.get("SQL_HOST", "localhost"),
-        "PORT": os.environ.get("SQL_PORT", "5432"),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation
